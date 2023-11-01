a3e173fbe8cede50159901fb4a706c20829fe96b
Glitch (glitch-hello-node) <none>
2023-11-01 05:34:09 +0000

    👓🙌 Checkpoint ./server.js:61839850/41 ./src/pages/riwayat.hbs:61839850/26

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ handlebars.registerHelper('noUrut', function (index) {
 })
 
 handlebars.registerHelper('warna', function(warna){
-  const pilihan = {"biru" : "primary", "abu" : "secondary", "hijau" : "success","jingga" : "info", "kuning" :"warning", "merah" : "danger", "putih" : "", "hitam"}
-  
+  const pilihan = {"biru" : "primary", "abu" : "secondary", "hijau" : "success","jingga" : "info", "kuning" :"warning", "merah" : "danger", "putih" : "light", "hitam" : "dark"}
+  return pilihan[warna]
 })
 
 function setCookie(exdays) {
